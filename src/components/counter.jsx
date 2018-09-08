adfca35c0455a8339812ac2fe5250c62297e6bee
Tio Saputra <karya.tiosaputra@gmail.com>
2018-09-08 17:26:06 +0700

    Handling Events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
-// Conditional Rendering
+// Rendering Lists
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags !</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Increment Clicked");
   }
 
   render() {
     return (
       // React.Fragment can be use if this component not returning parent E.g. div
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
