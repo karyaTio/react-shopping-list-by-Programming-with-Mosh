641923d12f3d9ec4fa5b452aee384673de51297e
Tio Saputra <karya.tiosaputra@gmail.com>
2018-09-08 14:29:21 +0700

    Rendering Classes Dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,18 +4,24 @@ class Counter extends Component {
   state = {
     count: 0
   };
-
+  // Rendering Classes Dynamically
   render() {
     return (
       // React.Fragment can be use if this component not returning parent E.g. div
       <div>
-        <span style={{ fontSize: 20 }} className="badge badge-primary m-2">
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
 
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
