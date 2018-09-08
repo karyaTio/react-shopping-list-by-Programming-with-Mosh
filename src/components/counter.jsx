1c84e793f49bb67338bf3163da76b19cb329ad9f
Tio Saputra <karya.tiosaputra@gmail.com>
2018-09-08 17:33:02 +0700

    What Happens When State Changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-// Binding Event Handlers
+// Updateing The State
 class Counter extends Component {
   state = {
     count: 0
   };
 
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
   handleIncrement = () => {
-    console.log("Increment Clicked", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
