061ccd8915ac267ac52fa817ea7b4bffa4b6bf10
Tio Saputra <karya.tiosaputra@gmail.com>
2018-09-08 14:23:55 +0700

    Setting Attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,8 +9,11 @@ class Counter extends Component {
     return (
       // React.Fragment can be use if this component not returning parent E.g. div
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={{ fontSize: 20 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
